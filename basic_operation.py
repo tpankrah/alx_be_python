df014eaa02dfcec2a104a66a85df8de40ac80ce6
tpankrah <tpankrah@live.com>
2025-07-13 19:56:55 +0000

    basic operations assignment

---
+++ basic_operation.py
@@ -0,0 +1,11 @@
+number1 = 10;
+number2 = 5;
+
+sum = number1 + number2;
+difference = number2 -  number1;
+product = number1 * number2;
+
+print(f"sum of {number1} and {number2} is {sum}");
+print (f"difference of  {number1} and {number2} is  {difference}"); 
+print("product of " + str(number1) + " and " + str(number2) + " is " + str(product));
+
