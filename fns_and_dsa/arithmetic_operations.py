1a4ee4088f013dde14c3f60f62f0af9813be8a88
tpankrah <tpankrah@live.com>
2025-07-27 21:00:58 +0000

    pushing again to main

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,3 @@
-#Requirements for arithmetic_operations.py:
-#Function Definition:
-#Define a function named perform_operation.
-#Parameters: num1 (float), num2 (float), and operation (string). 
-# The operation parameter accepts four possible values: 'add', 'subtract', 'multiply', or 'divide'.
-#The function should execute the arithmetic operation based on the operation parameter 
-# and the numerical values provided.
-#For division, include handling for division by zero, 
-# returning a specific message or value that your main.py script can recognize and display appropriately.
-#Return the result of the arithmetic operation.
-
 def perform_operation( num1 :float, num2 :float, operation :str):
    
     match operation:
