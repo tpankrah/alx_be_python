d0c4365e1742c5e474b5a707ef20eac5fc970106
tpankrah <tpankrah@live.com>
2025-07-29 17:21:51 +0000

    datetime operations

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -12,11 +12,11 @@ print(f'Current date and time: {display_current_datetime()}')
 
 number_of_days = int(input("Enter the number of days to add to the current date:"))
 
-//future_date =""
+
 
 def calculate_future_date(number_of_days):
     return (datetime.datetime.now() + datetime.timedelta(days=number_of_days))
 
-future_date =calculate_future_date(number_of_days)
+future_date = calculate_future_date(number_of_days)
 
 print(f' Future date: {future_date.strftime('%Y-%m-%d')}')
