fd4b35a0a7adc9015d833ec7dba17ed022839247
tpankrah <tpankrah@live.com>
2025-08-24 07:38:39 +0000

    sql book store assignment

---
+++ intro_to_DB/alx_book_store.sql
@@ -0,0 +1,62 @@
+
+-- mandatory
+-- Score: 0.0% (Checks completed: 0.0%)
+-- Imagine you’re tasked with designing a MySQL database for an online bookstore. 
+--The database should store information about books, authors, customers, orders, and order details. 
+--Here’s an overview of the database schema:
+
+-- Database Name: alx_book_store
+
+-- Books: Stores information about books available in the bookstore.
+CREATE DATABASE  alx_book_store;
+USE alx_book_store;
+
+
+CREATE TABLE  IF NOT EXISTS Authors (
+    `author_id` INT PRIMARY KEY,
+    `author_name` VARCHAR(215)
+);
+
+CREATE TABLE IF NOT EXISTS  Books (
+    `book_id` INT PRIMARY KEY,
+    `title` VARCHAR(130),
+    `author_id` INT, 
+    `price` DOUBLE,
+    `publication_date` DATE,
+    FOREIGN KEY (`author_id`) REFERENCES Authors(`author_id`)
+   
+);
+
+
+-- Customers: Stores information about customers.
+CREATE TABLE IF NOT EXISTS Customers (
+    `customer_id`  INT PRIMARY KEY,
+    `customer_name` VARCHAR(215),
+    `email` VARCHAR(215),
+    `address` TEXT
+);
+
+
+-- Orders: Stores information about orders placed by customers.
+CREATE TABLE IF NOT EXISTS Orders(
+`order_id` INT PRIMARY KEY,
+`customer_id` INT,
+`order_date` DATE,
+FOREIGN KEY (`customer_id`) REFERENCES Customers(`customer_id`)
+);
+-- Order_Details: Stores information about the books included in each order.
+
+CREATE TABLE IF NOT EXISTS order_details(
+`orderdetailid` INT PRIMARY KEY,
+`order_id` INT,
+`book_id` INT,
+`quantity` DOUBLE,
+FOREIGN KEY (`order_id`) REFERENCES Orders(`order_id`),
+FOREIGN KEY (`book_id`) REFERENCES Books(`book_id`)
+);
+
+
+-- Repo:
+
+-- GitHub repository: Intro_to_DB
+-- File: alx_book_store.sql
